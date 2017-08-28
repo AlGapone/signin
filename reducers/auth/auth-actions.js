0b4c5388a90e334b97d237bb89166753897d89f6
Yaroslav Hapon <yagapon@yandex.ru>
2017-08-28 14:34:20 +0300

    Update auth-actions

--- reducers/auth/auth-actions.js
+++ reducers/auth/auth-actions.js
@@ -1,5 +1,3 @@
-import { browserHistory } from 'react-router'
-
 export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
 export const SIGN_IN_REQUEST = 'SIGN_IN_REQUEST';
 export const SIGN_OUT = 'SIGN_OUT';
